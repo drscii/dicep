ee8f8735b3e65041efb328692ce32642ee7e524e
drscii <114014807+drscii@users.noreply.github.com>
2023-01-14 06:33:12 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,19 @@
+function roll()
+{
+  var num=Math.ceil(Math.random()*6);
+  var num1=Math.ceil(Math.random()*6);
+   //test alert(num);
+   document.getElementById('img0').setAttribute("src","images/dice"+num+".png");
+   document.getElementById('img02').setAttribute("src","images/dice"+num1+".png");
+if(num>num1)
+{
+  document.getElementById('refresh').innerHTML="Player 1 wins";
+}
+else if(num<num1)
+{
+  document.getElementById('refresh').innerHTML="Player 2 wins";
+}
+else{
+  document.getElementById('refresh').innerHTML="Nobody wins";
+}
+}
